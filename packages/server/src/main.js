d47e14fc60686861d43fe6d203010530db1b48cc
denilsonapf <denilsonapf@bitbucket.org>
2022-06-21 10:56:56 -0300

    first commit

---
+++ packages/server/src/main.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import { ApolloServer, gql } from "apollo-server-express";
+import typeDefs from "./graphql/typeDefs";
+import resolvers from "./graphql/resolvers";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+const app = express();
+
+async function startServer() {
+  const server = new ApolloServer({
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+
+  server.applyMiddleware({
+    app,
+    cors: { origin: "http://localhost:3000" },
+    bodyParserConfig: true,
+  });
+}
+
+startServer();
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || "127.0.0.1";
+
+app.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is listening at http://127.0.0.1:${PORT}`);
+});
